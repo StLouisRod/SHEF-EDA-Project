c9dec311f933ab59a51a18be7aa393e41d77048c
Rod Unterreiner <RodU.Business@gmail.com>
2026-05-16 12:50:36 -0500

    EDA initial code

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+    name="SHEF-EDA-PROJECT",
+    version="0.0.0",
+    author="Part",
+    author_mail="rod.unterreiner@gmail.com",
+    packages=find_packages()
+)
